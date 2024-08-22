87712ff875b8234da05e9cf157ee9c3845c940bc
kalpana Bharath <stonepanakeerthu@gmail.com>
2024-08-22 15:36:58 +0530

    class component

--- src/comonents/UserDetail/index.js
+++ src/comonents/UserDetail/index.js
@@ -1,17 +1,37 @@
 import "./index.css";
 
-let UserDetail = (props) => {
-  let { user } = props;
-  let { imageurl, name, role, key } = user;
-  return (
-    <li className="cardcointainer">
-      <img src={imageurl} className="image" alt="userpicture" />
-      <div className="text">
-        <h3>{name}</h3>
-        <p>{role}</p>
-      </div>
-    </li>
-  );
-};
+import { Component } from "react";
+
+class UserDetail extends Component {
+  render() {
+    let { user } = this.props;
+    let { imageurl, name, role, key } = user;
+
+    return (
+      <li className="cardcointainer">
+        <img src={imageurl} className="image" alt="userpicture" />
+        <div className="text">
+          <h3>{name}</h3>
+          <p>{role}</p>
+        </div>
+      </li>
+    );
+  }
+}
+
+//functional component
+// let UserDetail = (props) => {
+//   let { user } = props;
+//   let { imageurl, name, role, key } = user;
+//   return (
+//     <li className="cardcointainer">
+//       <img src={imageurl} className="image" alt="userpicture" />
+//       <div className="text">
+//         <h3>{name}</h3>
+//         <p>{role}</p>
+//       </div>
+//     </li>
+//   );
+// };
 
 export default UserDetail;
