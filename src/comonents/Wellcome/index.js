bfc6069e08ae9e05d00ec901d322a4d3dbd36cf1
kalpana Bharath <stonepanakeerthu@gmail.com>
2024-08-23 14:32:57 +0530

    conditional rendering Element Variable

--- src/comonents/Wellcome/index.js
+++ src/comonents/Wellcome/index.js
@@ -2,21 +2,21 @@ import { Component } from "react";
 import "./index.css";
 
 class Wellcome extends Component {
-  state = { isLogedIn: false };
-
-  authobuttion = () => {
-    let { isLogedIn } = this.state;
-    if (isLogedIn === true) {
-      return <button> Logout</button>;
-    }
-    return <button>log In</button>;
-  };
+  state = { isLogedIn: true };
 
   render() {
+    let { isLogedIn } = this.state;
+    let authbuttion;
+    if (isLogedIn === true) {
+      authbuttion = <button>Logout</button>;
+    } else {
+      authbuttion = <button>LogIn</button>;
+    }
+
     return (
       <div className="wellcomecontainer">
         <h3>Hello Kalpana</h3>
-        {this.authobuttion()}
+        {authbuttion}
       </div>
     );
   }
